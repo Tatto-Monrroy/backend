a571040dd624ec57f84e786f3911f32171423a2b
Estuardo Monrroy <estuardo.monrroy2@gmail.com>
2018-07-28 23:41:17 -0600

    Entity usuario creado

---
+++ src/main/java/com/proyecto/demo/model/Usuario.java
@@ -0,0 +1,107 @@
+package com.proyecto.demo.model;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+@Access(AccessType.FIELD)
+public class Usuario extends ParentEntity {
+
+	private static final long serialVersionUID = -1272953663537907477L;
+	
+	@Column(name = "primer_nombre", nullable = false, length = 255)
+	private String primerNombre;
+	
+	@Column(name = "segundo_nombre", nullable = true, length = 255)
+	private String segundoNombre;
+	
+	@Column(name = "apellido", nullable = false, length = 255)
+    private String apellido;
+	
+	@Column(name = "segundo_apellido", nullable = true, length = 255)
+	private String segundoApellido;
+	
+	@Column(name = "telefono", nullable = true, length = 255)
+	private String telefono;
+	
+	@Column(name = "direccion", nullable = false, length = 255)
+    private String direccion;
+    
+	/**
+	 * @return the primerNombre
+	 */
+	public String getPrimerNombre() {
+		return primerNombre;
+	}
+	/**
+	 * @param primerNombre the primerNombre to set
+	 */
+	public void setPrimerNombre(String primerNombre) {
+		this.primerNombre = primerNombre;
+	}
+	/**
+	 * @return the segundoNombre
+	 */
+	public String getSegundoNombre() {
+		return segundoNombre;
+	}
+	/**
+	 * @param segundoNombre the segundoNombre to set
+	 */
+	public void setSegundoNombre(String segundoNombre) {
+		this.segundoNombre = segundoNombre;
+	}
+	/**
+	 * @return the apellido
+	 */
+	public String getApellido() {
+		return apellido;
+	}
+	/**
+	 * @param apellido the apellido to set
+	 */
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	/**
+	 * @return the segundoApellido
+	 */
+	public String getSegundoApellido() {
+		return segundoApellido;
+	}
+	/**
+	 * @param segundoApellido the segundoApellido to set
+	 */
+	public void setSegundoApellido(String segundoApellido) {
+		this.segundoApellido = segundoApellido;
+	}
+	/**
+	 * @return the telefono
+	 */
+	public String getTelefono() {
+		return telefono;
+	}
+	/**
+	 * @param telefono the telefono to set
+	 */
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	/**
+	 * @return the direccion
+	 */
+	public String getDireccion() {
+		return direccion;
+	}
+	/**
+	 * @param direccion the direccion to set
+	 */
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+}
